078714e825ce249b688300a1fce7ea0f466c46e7
Richie Bendall <richiebendall@gmail.com>
2019-03-14 18:13:12 +1300

    Update code

--- darkdetector.js
+++ darkdetector.js
@@ -1,5 +1,21 @@
-const DarkDetector = () => {
-    return window.matchMedia("prefers-dark-interface").matches || document.getElementsByTagName("body")[0].classList.contains("dm-dark")
+const DarkDetector = ({
+    query = true,
+    bodyclass = false,
+    time = false
+}) => {
+    // CSS media query
+    if (query && window.matchMedia('(prefers-color-scheme: dark)').matches) return true
+
+    // Class on body
+    if (bodyclass && document.getElementsByTagName("body")[0].classList.contains(typeof bodyclass === "boolean" ? "dm-dark" : bodyclass)) return true
+
+    // Night time
+    if (time && new Date().getHours() >= time.min ? time.min : 6 && new Date().getHours() <= time.max ? time.max : 20) return true
+
+    // If all checks fail
+    return false
 }
 
-module.exports = {DarkDetector}
+export default {
+    DarkDetector
+}
